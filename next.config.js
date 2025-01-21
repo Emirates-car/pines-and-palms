006cb5b459979168ac982b240b2ef5d8e27bddd0
Emirates-car <emiratesautomobileparts@gmail.com>
2025-01-20 18:38:00 -0800

    commit

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ module.exports = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'production',
   },
-  staticPageGenerationTimeout: 5000,
+  staticPageGenerationTimeout: 10000,
 });

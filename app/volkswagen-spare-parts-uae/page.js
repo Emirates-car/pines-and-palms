6f8cbc8aeaa31b5099f92045ac3bca091c6a04c5
Afrozefathima <jsafroze@gmail.com>
2025-08-12 20:48:40 +0400

    commit

--- app/volkswagen-spare-parts-uae/page.js
+++ app/volkswagen-spare-parts-uae/page.js
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
+import VolkswagenClient from './VolkswagenClient';
 
 export default function Page() {
     return (
         <Suspense fallback={<div>Loading filters...</div>}>
-            <VolkswagenParts />
+            <VolkswagenClient />
         </Suspense>
     );
 }

e3b2561c7283d787db60be89e328a21f5aa2b2a3
Afrozefathima <jsafroze@gmail.com>
2025-10-27 23:01:05 +0400

    commit

--- app/search-by-make/[make]/page.js
+++ app/search-by-make/[make]/page.js
@@ -804,52 +804,7 @@ export default async function MakePage({ params, searchParams }) {
           ))}
         </ul>
       </section>
-      <section aria-label="Latest Received Inquiries" className="pt-10">
-        <h2 className="text-black text-4xl text-center md:text-lg lg:text-2xl font-extrabold xs:text-xl 2xs:text-xs">
-          <span className="text-red-600 animate-bounce inline-block">Latest</span> Received Inquiry
-        </h2>
-
-        <div className="pt-10 xl:mx-36 lg:mx-10 md:mx-10 sm:mx-5 xs:mx-2 2xs:mx-2 s:mx-2 md:ml-11 my-10 mx-10 overflow-x-auto">
-          <table
-            className="table-auto w-full border-collapse border border-gray-800"
-            aria-label="Recent Car Part Inquiries"
-          >
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="px-4 py-2 bg-gray-800 text-white text-left"
-                >
-                  Car
-                </th>
-                <th
-                  scope="col"
-                  className="px-4 py-2 bg-gray-800 text-white text-left"
-                >
-                  Part List
-                </th>
-                <th
-                  scope="col"
-                  className="px-4 py-2 bg-gray-800 text-white text-left"
-                >
-                  Car Location
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {latestInquiry.map((h, i) => (
-                <tr key={i}>
-                  <td className="border font-semibold px-4 py-2">
-                    {h.Year} {h.BRAND} {h.Model}
-                  </td>
-                  <td className="border px-4 py-2">{h.PartList}</td>
-                  <td className="border px-4 py-2">{h.Location}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </section>
+      <TenEntries />
       <PartsAccordion make={make} />
 
       <section className='mx-3' aria-labelledby={`How to buy ${make} parts`}>

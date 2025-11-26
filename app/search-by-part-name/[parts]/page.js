a20e067a9798ef00f720374487137ce99fdf3c69
Afrozefathima <jsafroze@gmail.com>
2025-11-26 21:54:36 +0400

    commit

--- app/search-by-part-name/[parts]/page.js
+++ app/search-by-part-name/[parts]/page.js
@@ -11,7 +11,21 @@ import Counter from '../../../components/service-countup';
 import path from 'path';
 import { promises as fs } from 'fs';
 import { notFound } from 'next/navigation';
+import products from "../../../public/products.json"
+import ProductFilter from './ProductFilter';
+import { Fira_Sans, Playfair_Display } from 'next/font/google';
+const playfair_display = Playfair_Display({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-playfair-display',
+});
 
+const firaSans = Fira_Sans({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-fira-sans',
+});
 
 export async function generateStaticParams() {
   try {
@@ -33,13 +47,15 @@ export async function generateStaticParams() {
 export async function generateMetadata({ params }) {
   const { parts } = params;
   const decodedParts = decodeURIComponent(parts);
+
+
   return {
-    title: `${decodedParts} Auto Spare Parts Order Online in UAE | Emirates-car.com`,
-    description: `Buy Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in ${decodedParts} UAE`,
+    title: `${decodedParts} Car Spare Parts Order Online in UAE | EMIRATESCAR`,
+    description: `Buy ${decodedParts}  Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in UAE`,
     openGraph: {
       images: 'https://emirates-car.com/favicon.png',
-      title: `${decodedParts} Auto Spare Parts Order Online in UAE | Emirates-car.com`,
-      description: `Buy Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in ${decodedParts} UAE`,
+      title: `${decodedParts} Car Spare Parts Order Online in UAE | EMIRATESCAR`,
+      description: `Buy ${decodedParts} Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in UAE`,
       url: 'https://emirates-car.com/search-by-part-name/' + parts,
       image: 'https://emirates-car.com/img/car-spare-parts.png',
       siteName: 'Emirates Auto Parts',
@@ -61,9 +77,9 @@ export async function generateMetadata({ params }) {
     },
     twitter: {
       card: 'summary_large_image',
-      title: `${decodedParts} Auto Spare Parts Order Online in UAE - Used, Genuine, Aftermarket | Emirates-car.com`,
+      title: `${decodedParts} Car Spare Parts Order Online in UAE - Used, Genuine, Aftermarket | Emirates-car.com`,
       url: 'https://www.emirates-car.com/search-by-part-name/' + parts,
-      description: `Buy Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in ${decodedParts} UAE`,
+      description: `Buy ${decodedParts} Online and Get delivered Used, New, Genuine / Original / OEM, Aftermarket auto spare parts Online in UAE`,
       images: ['https://emirates-car.com/favicon.png'],
     },
     icons: {
@@ -92,9 +108,16 @@ async function getPartsData(parts) {
 
   return filtered;
 }
-export default async function Parts({ params }) {
+export default async function Parts({ params, searchParams }) {
   const { parts } = params;
   const partsData = await getPartsData(parts);
+  const staticcity = [
+    "Abu Dhabi", "Dubai", "Sharjah", "Deira", "Ajman", "Al Ain", "Al Quoz", "Al Fujairah"
+  ]
+  let filteredProducts = products.filter(
+    (p) =>
+      p.subcategory.toLowerCase() === parts.toLowerCase()
+  );
 
   if (!partsData || partsData.length === 0) {
     notFound();
@@ -105,9 +128,58 @@ export default async function Parts({ params }) {
   const partsposts = await getParts();
   const modelsform = await getFormModel();
 
+  const {
+    "filter_car_parts[]": categories = [],
+    "engine[]": engines = [],
+    "compatibility[]": compats = [],
+    search = "",
+  } = searchParams || {};
+
+  const selectedCategories = Array.isArray(categories)
+    ? categories
+    : categories
+      ? [categories]
+      : [];
+
+  const selectedEngines = Array.isArray(engines)
+    ? engines
+    : engines
+      ? [engines]
+      : [];
+
+  const selectedCompats = Array.isArray(compats)
+    ? compats
+    : compats
+      ? [compats]
+      : [];
+
+
+  // STEP 3 — Filter based on searchParams
+  if (selectedCategories.length > 0) {
+    filteredProducts = filteredProducts.filter((p) =>
+      selectedCategories.includes(p.category)
+    );
+  }
+
+  if (selectedEngines.length > 0) {
+    filteredProducts = filteredProducts.filter((p) =>
+      p.engine?.some((e) => selectedEngines.includes(e))
+    );
+  }
+
+  if (selectedCompats.length > 0) {
+    filteredProducts = filteredProducts.filter((p) =>
+      p.compatibility?.some((c) =>
+        selectedCompats.includes(
+          `${c.make} ${c.model} ${c.years ? `(${c.years})` : ""}`
+        )
+      )
+    );
+  }
+
 
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <div className="py-5 xxs:px-7 sm:px-7 s:py-6 lg:mx-6 md:mx-6 xs:mx-2 xxs:mx-2 max-w-7xl mx-auto">
         <div className="bg-backgroundlight rounded-sm">
           <div className="grid grid-cols-2 xs:grid xs:grid-cols-1 s:grid s:grid-cols-1 xs:text-center sm:grid sm:grid-cols-2 xxs:grid xxs:grid-cols-1 xs:pt-5 s:pt-5">
@@ -153,10 +225,18 @@ export default async function Parts({ params }) {
           </div>
         </div>
       </div>
+      <ProductFilter
+        products={filteredProducts}
+        allProducts={products}
+        searchParams={searchParams || {}}
+      />
+      <section>
+        <FormComponent formsData={modelsform} postFilter={partsposts} />
+      </section>
       <div className="flex xs:grid xs:grid-cols-1 s:grid s:grid-cols-1 sm:grid sm:grid-cols-1 xxs:grid xxs:grid-cols-1">
-        <main>
+        <div>
           <div className="grid grid-cols-1 s:grid s:grid-cols-1 xs:grid xs:grid-cols-1 xxs:grid xxs:grid-cols-1 sm:grid sm:grid-cols-1">
-            <FormComponent formsData={modelsform} postFilter={partsposts} />
+
             <div className="p-5 pt-10">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722504.3860201286!2d51.71183150969869!3d24.337497293019872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e48dfb1ab12bd%3A0x33d32f56c0080aa7!2sUnited%20Arab%20Emirates!5e0!3m2!1sen!2sin!4v1641654109734!5m2!1sen!2sin"
@@ -170,59 +250,73 @@ export default async function Parts({ params }) {
             </div>
           </div>
 
-          <h3 className="text-black text-4xl text-center md:text-2xl lg:text-2xl font-bold xs:text-xl xxs:text-2xl pt-10">
-            Search{' '}
-            <span className="text-blue-500">
-              {decodeURIComponent(partsData.parts)}{' '}
-            </span>
-            in UAE
-          </h3>
-          <div className="grid grid-cols-7 md:grid-cols-5 lg:grid-cols-7 md:mx-4 sm:mx-3 xs:grid xs:grid-cols-2 sm:grid sm:grid-cols-6 xxs:grid xxs:grid-cols-2 s:grid s:grid-cols-2 gap-1 xs:mx-4 s:mx-4 xxs:mx-4 md:ml-11 my-10 mx-10">
-            {makedatas.map((post, i) => (
-              <div key={i}>
-                <Link
-                  href="/search-by-make/[make]"
-                  as={'/search-by-make/' + post.make}
-                  title={post.make + ' ' + decodeURIComponent(partsData.parts)}
-                >
-                  <main className="border h-full  hover:border-blue-600 py-3 bg-gray-100 rounded-sm">
-                    <div className=" text-center font-sans ">
-                      {post.make} {decodeURIComponent(partsData.parts)}
-                    </div>
-                  </main>
-                </Link>
-              </div>
-            ))}
-          </div>
+          <section
+            aria-labelledby={`${parts}`}
+            className="mt-10 shadow-sm mx-4 md:mx-4 lg:max-w-4xl lg:mx-auto xl:mx-10 bg-bglight px-5 md:px-20 lg:px-10"
+          >
+            <h2
+              id={`${parts}`}
+              className={`text-4xl md:text-3xl lg:text-3xl xs:text-2xl xxs:text-2xl font-semibold py-5 ${playfair_display.className}`}
+            >
+              Used, Genuine & Aftermarket <span className="text-blue-600">{parts}</span>
+            </h2>
+
+            <ul className="grid grid-cols-6 md:grid-cols-5 xs:grid-cols-2 xxs:grid-cols-3 sm:grid-cols-3 xs:gap-2 xxs:gap-2 sm:gap-2 gap-4 my-10">
+              {makedatas.map((p, i) => (
+                <li key={i} className="list-none">
+                  <Link
+                    href="/search-by-make/[make]/parts/[parts]"
+                    as={`/search-by-make/${p.make}/parts/${partsData.parts}`}
+                    title={`${p.make} ${parts}`}
+                    target='_blank'
+                    className="flex flex-col items-center justify-center border hover:border-blue-600 p-3 rounded-sm bg-white"
+                  >
+                    <Image
+                      alt={`${p.make} parts`}
+                      src={`/img/car-logos/${p.img}`}
+                      height={90}
+                      width={90}
+                      className="object-contain"
+                      priority
+                    />
+                    <span className={`mt-2 px-3 py-1 text-sm xl:text-2xl xxl:text-2xl font-medium font-sans rounded-sm text-center w-max ${firaSans.className}`}>
+                      <span className='text-blue-500'>{p.make}</span> {parts}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
           <TenEntries />
           <Counter />
-          <h3 className="text-black text-4xl text-center md:text-2xl lg:text-2xl font-bold xs:text-xl xxs:text-2xl pt-10">
-            Search{' '}
-            <span className="text-blue-500">
-              {decodeURIComponent(partsData.parts)}{' '}
-            </span>
-            parts in UAE
-          </h3>
-          <div className="grid grid-cols-5 md:grid-cols-5 lg:grid-cols-7 md:mx-4 sm:mx-3 xs:grid xs:grid-cols-2 sm:grid sm:grid-cols-6 xxs:grid xxs:grid-cols-2 s:grid s:grid-cols-2 gap-1 xs:mx-4 s:mx-4 xxs:mx-4 md:ml-11 my-10 mx-10">
-            {cities.map((post, i) => (
-              <div key={i}>
-                <Link
-                  href="/search-by-cities-in-uae/[city]"
-                  as={'/search-by-cities-in-uae/' + post.city}
-                  title={
-                    decodeURIComponent(partsData.parts) + ' in ' + post.city
-                  }
-                >
-                  <div className="border h-full  hover:border-blue-600 py-3 bg-gray-100 rounded-sm">
-                    <div className=" text-center font-sans ">
-                      {decodeURIComponent(partsData.parts)} in {post.city}
+          <section>
+            <h3 className="text-black text-4xl text-center md:text-2xl lg:text-2xl font-bold xs:text-xl xxs:text-2xl pt-10">
+              Search{' '}
+              <span className="text-blue-500">
+                {decodeURIComponent(partsData.parts)}{' '}
+              </span>
+              parts in UAE
+            </h3>
+            <ul className="grid grid-cols-5 md:grid-cols-5 lg:grid-cols-7 md:mx-4 sm:mx-3 xs:grid xs:grid-cols-2 sm:grid sm:grid-cols-6 xxs:grid xxs:grid-cols-2 s:grid s:grid-cols-2 gap-1 xs:mx-4 s:mx-4 xxs:mx-4 md:ml-11 my-10 mx-10">
+              {staticcity.map((post, i) => (
+                <li key={i}>
+                  <Link
+                    href="/search-by-cities-in-uae/[city]"
+                    as={'/search-by-cities-in-uae/' + post}
+                    title={
+                      decodeURIComponent(partsData.parts) + ' in ' + post
+                    }
+                  >
+                    <div className={`border h-full text-sm xl:text-2xl xxl:text-2xl font-medium font-sans rounded-sm text-center hover:border-blue-600 py-3 bg-gray-100 rounded-sm ${firaSans.className}`}>
+                      {decodeURIComponent(partsData.parts)} in <span className='text-blue-500'>{post}</span>
                     </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </main>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+
+        </div>
       </div>
     </div>
   );
